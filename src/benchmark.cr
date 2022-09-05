37a9985bec9e6df46324e26be66129951a5fc984
soya_daizu <soya_daizu@protonmail.com>
2022-09-06 04:52:12 +0900

    Include build time to the benchmark runner

--- src/benchmark.cr
+++ src/benchmark.cr
@@ -37,28 +37,36 @@ def perform(command : String)
   avg
 end
 
-def plot_graph(program : String, results : Hash(String, Time::Span))
+def plot_graph(program : String, exec_times : Hash(String, Time::Span), build_times : Hash(String, Time::Span))
   File.open("media/#{program}.png", "w") do |file|
     Ishi.new(file) do
-      x = (1..results.size).to_a
-      y = results.values.map(&.to_f)
-      xtics = results.keys.map_with_index { |m, i| {(i + 1).to_f, m} }.to_h
+      x = (1..exec_times.size).to_a
+      exec_y = exec_times.values.map(&.to_f)
+      build_y = exec_times.map do |k, v|
+        (v + (build_times[k]? || Time::Span.zero)).to_f
+      end
+      xtics = exec_times.keys.map_with_index { |m, i| {(i + 1).to_f, m} }.to_h
 
-      plot(x, y, title: "Execution time (sec.)", style: :boxes, fs: 0.25)
+      plot(x, build_y, title: "Build time (sec.)", style: :boxes, fs: 0.25)
         .boxwidth(0.5)
-        .ylabel("Execution time (5 times avg.)")
+      plot(x, exec_y, title: "Execution time (sec.)", style: :boxes, fs: 0.25)
+        .boxwidth(0.5)
+        .ylabel("Time spent (5 times avg.)")
         .xlabel("Mode")
         .xtics(xtics)
     end
   end
 end
 
-def plot_table(results : Hash(String, Time::Span))
+def plot_table(exec_times : Hash(String, Time::Span), build_times : Hash(String, Time::Span))
   titles = [] of String
   values = [] of String
 
-  results.each do |key_str, time|
-    time_str = time.to_f.round(3).to_s
+  exec_times.each do |key_str, time|
+    time_str = "#{time.to_f.round(3).to_s}s"
+    if build_time = build_times[key_str]?
+      time_str += " (+ #{build_time.to_f.round(3)}s build time)"
+    end
     length_diff = key_str.size - time_str.size
 
     if length_diff > 0
@@ -70,12 +78,10 @@ def plot_table(results : Hash(String, Time::Span))
     titles.push(key_str)
     values.push(time_str)
   end
-  h_separators = titles.map(&.gsub(/./, '-'))
 
   String.build do |str|
-    str << "| " << titles.join(" | ") << " |" << '\n'
-    str << "| " << h_separators.join(" | ") << " |" << '\n'
-    str << "| " << values.join(" | ") << " |" << '\n'
+    str << "| Mode | " << titles.join(" | ") << " |" << '\n'
+    str << "| Time | " << values.join(" | ") << " |" << '\n'
   end
 end
 
@@ -86,7 +92,8 @@ programs = Dir.glob("samples/*.bf")
 programs.each do |program_path|
   program_name = program_path.match(/\w+\/(\w+)\.bf/).try(&.[1]) || "unknown"
   puts "PROGRAM: #{program_name}.bf"
-  results = {} of String => Time::Span
+  exec_times = {} of String => Time::Span
+  build_times = {} of String => Time::Span
 
   input = if program_name == "factor"
             %q(echo "179424691\n" | )
@@ -94,26 +101,26 @@ programs.each do |program_path|
             ""
           end
 
-  results["brainfuck.cr"] = perform("#{input}./patched_brainfuck #{program_path}")
+  exec_times["brainfuck.cr"] = perform("#{input}./patched_brainfuck #{program_path}")
 
-  results["run"] = perform("#{input}bin/bfcr run #{program_path}")
+  exec_times["run"] = perform("#{input}bin/bfcr run #{program_path}")
 
-  results["run (no opt)"] = perform("#{input}bin/bfcr run #{program_path} --no-opt")
+  exec_times["run (no opt)"] = perform("#{input}bin/bfcr run #{program_path} --no-opt")
 
-  results["jit"] = perform("#{input}bin/bfcr jit #{program_path}")
+  exec_times["jit"] = perform("#{input}bin/bfcr jit #{program_path}")
 
-  results["jit (no opt)"] = perform("#{input}bin/bfcr jit #{program_path} --no-opt")
+  exec_times["jit (no opt)"] = perform("#{input}bin/bfcr jit #{program_path} --no-opt")
 
   puts "Building #{program_name}.bf ..."
-  `bin/bfcr build #{program_path}`
+  build_times["build"] = Benchmark.realtime { `bin/bfcr build #{program_path}` }
   raise "Building #{program_name}.bf failed" unless $?.success?
-  results["build"] = perform("#{input}./out")
+  exec_times["build"] = perform("#{input}./out")
 
   puts "Building #{program_name}.bf --no-opt ..."
-  `bin/bfcr build #{program_path} --no-opt`
+  build_times["build (no opt)"] = Benchmark.realtime { `bin/bfcr build #{program_path} --no-opt` }
   raise "Building #{program_name}.bf --no-opt failed" unless $?.success?
-  results["build (no opt)"] = perform("#{input}./out")
+  exec_times["build (no opt)"] = perform("#{input}./out")
 
-  plot_graph(program_name, results)
-  puts plot_table(results)
+  plot_graph(program_name, exec_times, build_times)
+  puts plot_table(exec_times, build_times)
 end
